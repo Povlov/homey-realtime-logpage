3b6d7505ebb43fab89df592eb4f1458e5e4fec4f
Povlov <povlov@gmail.com>
2017-04-23 15:58:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 setup = {
   ip: '0.0.0.0', // Put your ip here or athom cloud uri,
-  bearer_token: 'your token', // Put your bearer token here
+  bearer_token: '123456789', // Put your bearer token here
   protocol: 'http' // Change to https if using athom cloud
 }
